9a505c0bf5b443f8107827c8ef215728a6f34470
Claire Galya <clairegalya@Claires-MBP.fios-router.home>
2017-07-31 12:31:34 -0400

    comments in js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,11 +1,14 @@
 $(document).ready(function() {
 
+//button array
 var topics = ["funny cat", "silly cat", "grumpy cat", "sleepy cat", "cats in boxes", "kittens", "cute cat", "cats in hats"];
 
+//set up on page load
 searchGifs("cat");
 makeButtons();
 $("#submitBtn").attr("disabled", true);
 
+//disable make search button button so empty buttons arent made if it's pressed when text field is empty
 $("#addSearchTerm").keyup(function() {
 	if($(this).val().length != 0) {
 		$("#submitBtn").attr("disabled", false);
@@ -14,6 +17,7 @@ $("#addSearchTerm").keyup(function() {
 	}
 })
 
+//make a new search button by adding the new words to the array and calling the makeButtons function again
 function newButton() {
 	var newBtn = $("#addSearchTerm").val();
 	topics.push(newBtn);
@@ -22,11 +26,13 @@ function newButton() {
 	searchGifs(newBtn);
 }
 
+//when you press make a new button button, it calls newButton function to make a new button
 $("#submitBtn").click(function() {
 	newButton();
 	$("#submitBtn").attr("disabled", true);
 })
 
+//if you press enter instead of pressing the button, it still calls newButton
 $("input").keyup(function(){  
   if (event.keyCode == 13) {
    	newButton();
@@ -34,10 +40,11 @@ $("input").keyup(function(){
   }
 });
 
+//makes search buttons from the array of topics, including new topics submitted via the input box
 function makeButtons() {
 	$("#buttonDiv").empty()
 	for (var i = 0; i < topics.length; i++) {
-		var btnVal = topics[i].replace(/\s/, "+");
+		var btnVal = encodeURIComponent(topics[i]);
 		$("#buttonDiv").append("<button value='" + btnVal + "'>" + topics[i] + "</button>");
 	} 
 	$("button").click(function() {
@@ -45,6 +52,7 @@ function makeButtons() {
 	})
 }
 
+//uses giphy api to search for gifs based on search button criteria, limit is 100 results, results are shuffled so there aren't repeats in each individual search
 function searchGifs(searchTerm) {
 	$.ajax({
 		url: "https://api.giphy.com/v1/gifs/search?q=" + searchTerm + "&api_key=dc6zaTOxFJmzC&limit=100&rating=",
@@ -58,19 +66,20 @@ function searchGifs(searchTerm) {
 			var staticGif = response.data[i].images.fixed_height_still.url;
 			var animatedGif = response.data[i].images.fixed_height.url;
 			var rating = response.data[i].rating;
-			$("<div class='col-lg-3 col-md-4 col-xs-6 thumb'><div id='gifRating' class='thumbnail'><img src='" + staticGif + "' alt='' value='0' data='" + animatedGif + "'/><p id='rating'>rating: " + rating + "</p></div></div>").appendTo($("#gifDiv"));
+			$("<div class='col-lg-3 col-md-4 col-xs-6 thumb'><div id='gifRating' class='thumbnail'><img src='" + staticGif + "' alt='' value='0' data-alt='" + animatedGif + "'/><p id='rating'>rating: " + rating + "</p></div></div>").appendTo($("#gifDiv"));
 			}
+			//switches image source from static to animated and back again on click
 			$("img").click(function() {
 				var val = $(this).attr("value");
-				var data = $(this).attr("data");
+				var data = $(this).attr("data-alt");
 				var src = $(this).attr("src"); 
 				if (val == 0) {
 					$(this).attr("src", data);
-					$(this).attr("data", src);
+					$(this).attr("data-alt", src);
 					$(this).attr("value", "1");
 				} else {
 					$(this).attr("src", data);
-					$(this).attr("data", src);
+					$(this).attr("data-alt", src);
 					$(this).attr("value", "0");
 				}				
 			})
